41dd93e332f1ac5e1944538792a95bed62c22198
Hagen Hoffmann <hagen.hoffmann@interactive-tools.de>
2023-02-25 15:25:43 +0100

    set manual headers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     routeRules: {
-        '/isr': { swr: 120 },
+        '/isr': { swr: true, headers: { 'cache-control': 's-maxage=30' } },
         '/static': { static: true }
     }
 })
