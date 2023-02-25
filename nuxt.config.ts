0e4a76f36f4381648cfe47217d41510f4f60c1a9
Hagen Hoffmann <hagen.hoffmann@interactive-tools.de>
2023-02-25 13:18:56 +0100

    change swr to 30

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     routeRules: {
-        '/mightchange': { swr: true },
+        '/mightchange': { swr: 30 },
         '/about': { static: true }
     }
 })
