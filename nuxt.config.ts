69998daacc61eec904e39498c2f57355dd2e6088
Hagen Hoffmann <hagen.hoffmann@interactive-tools.de>
2023-02-25 14:59:42 +0100

    increase TTL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     routeRules: {
-        '/isr': { swr: 5 },
+        '/isr': { swr: 120 },
         '/static': { static: true }
     }
 })
