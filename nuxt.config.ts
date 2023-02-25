9dd7285dcd7b41ba66c551357e344c2b42981ea6
Hagen Hoffmann <hagen.hoffmann@interactive-tools.de>
2023-02-25 15:38:36 +0100

    set ttl back to 30

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     routeRules: {
-        // '/isr': { swr: true, headers: { 'cache-control': 's-maxage=30' } },
+        '/isr': { swr: 30 },
         '/static': { static: true }
     }
 })
